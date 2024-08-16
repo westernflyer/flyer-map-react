00d63620f0128340c6fba3be15607acc0ea1da52
Tom Keffer <tkeffer@gmail.com>
2024-08-15 18:30:00 -0700

    Checkpoint.

--- TODO.md
+++ TODO.md
@@ -1,9 +1,11 @@
 # TODO
 
 ## Table related issues
+
 - Predetermine table ordering
+- Need a higher-level context that holds the vessel's position, so it can be used by both the map and the table.
+- eslint says: "useEffect has missing dependencies: 'formattedState' and 'vesselState'. Either include them or remove
+  the dependency array. Outer scope values like 'client' aren't valid dependencies because mutating them doesn't
+  re-render the component"
 
 Post a notice if the MQTT broker disconnects.
-
-Could also use a refactor.
-
--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,99 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import { useState, useEffect, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./App.css";
 
-import mqtt from 'mqtt';
-import {getUpdateDicts, VesselState, FormattedState} from "./utilities.js";
-import { signalKUnits} from "./units.js";
+import mqtt from "mqtt";
+import { APIProvider, Map, AdvancedMarker } from "@vis.gl/react-google-maps";
+import DataTable from "react-data-table-component";
 
-import DataTable from 'react-data-table-component';
+import { getUpdateDicts, VesselState, FormattedState } from "./utilities.js";
+import { signalKUnits } from "./units.js";
 
 const tableColumns = [
     {
-        name: 'Path',
-        selector: row => row.key,
+        name: "Path",
+        selector: (row) => row.key,
     },
     {
-        name: 'Property',
-        selector: row => row.label,
+        name: "Property",
+        selector: (row) => row.label,
     },
     {
-        name: 'Value',
-        selector: row => row.value,
+        name: "Value",
+        selector: (row) => row.value,
     },
     {
-        name: 'Last update',
-        selector: row => row.last_update,
+        name: "Last update",
+        selector: (row) => row.last_update,
     },
-]
+];
 
-let client = mqtt.connect(
-    'ws://localhost:8080',
-    {clientId: "flyer-client-" + Math.floor(Math.random() * 10000)}
-);
+let client = mqtt.connect("ws://localhost:8080", {
+    clientId: "flyer-client-" + Math.floor(Math.random() * 10000),
+});
 
 // Subscribe to all the topics we know about
-Object.keys(signalKUnits).forEach(key => {
-    client.subscribe("signalk/+/" + key)
-})
+Object.keys(signalKUnits).forEach((key) => {
+    client.subscribe("signalk/+/" + key);
+});
 
-function App() {
+function VesselTable() {
     // Sets default React state
     const [vesselState, setVesselState] = useState(new VesselState());
     const [formattedState, setFormattedState] = useState(new FormattedState());
     useEffect(() => {
         if (client) {
-            client.on('message', function (topic, message) {
-                const updateDicts = getUpdateDicts(JSON.parse(message.toString()));
-                vesselState.mergeUpdates(updateDicts)
+            client.on("message", function (topic, message) {
+                const updateDicts = getUpdateDicts(
+                    JSON.parse(message.toString()),
+                );
+                vesselState.mergeUpdates(updateDicts);
                 setVesselState(new VesselState(vesselState));
                 formattedState.mergeUpdates(updateDicts);
                 setFormattedState(new FormattedState(formattedState));
-            })
+            });
         }
     }, [client]);
 
-
     return (
-        <div className="App">
-            <header className="App-header">
-                <div className="table-style">
-                    <DataTable
-                        data={Object.values(formattedState)}
-                        columns={tableColumns}
-                        title={"Current values"}
-                        responsive
-                    />
-                </div>
-            </header>
-        </div>
+        <DataTable
+            data={Object.values(formattedState)}
+            columns={tableColumns}
+            title={"Current values"}
+            responsive
+        />
     );
-
 }
+
+const App = () => (
+    <div style={{ height: "400px", width: "100%", padding: "50px" }}>
+        <header className="entry-header">
+            <h1 className="entry-title">Where is Flyer?</h1>
+        </header>
+        <StrictMode>
+            <APIProvider
+                apiKey={"AIzaSyBylqNRwGbPrkbK8oh6E9y_fzyNaZumyRs"}
+                onLoad={() => console.log("Maps API has loaded.")}
+            >
+                <Map
+                    defaultZoom={10}
+                    defaultCenter={{lat: 36.8, lng: -121.9}}
+                    mapId="DEMO_MAP_ID"
+                >
+                    <AdvancedMarker
+                        key={"flyer"}
+                        position={null}
+                    />
+                </Map>
+            </APIProvider>
+            <div style={{ padding: "20px" }}>
+                <VesselTable />
+            </div>
+        </StrictMode>
+    </div>
+);
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
 
 export default App;
