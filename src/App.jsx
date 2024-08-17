be7257e04ec7eed715c1496ce9240ff2dfb36683
Tom Keffer <tkeffer@gmail.com>
2024-08-17 13:36:34 -0700

    Add copyright notice

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2024-present Tom Keffer <tkeffer@gmail.com>
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { useState, useEffect, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./App.css";
