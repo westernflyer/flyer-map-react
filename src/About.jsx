1d5720ac9f43deb23f445e5104bcd302ee1e6910
Tom Keffer <tkeffer@gmail.com>
2024-12-04 15:45:53 -0800

    Update the map guide

--- src/About.jsx
+++ src/About.jsx
@@ -11,23 +11,23 @@ const MapGuide = () => {
     return (
         <>
             <h2>Map guide</h2>
+            <img src={"/flyer-map/boat sample.png"}
+                 style={{float: "right"}}
+                 width="155"
+                 height="173"
+                 alt="Image interpreting boat icon" />
             <p>
-                The black line is a <a
-                href={"https://www.weather.gov/hfo/windbarbinfo"}>wind
+                The black line is a <a href={"https://www.weather.gov/hfo/windbarbinfo"}>wind
                 barb</a> that shows the wind direction and speed. It
                 points&nbsp;<i>from</i>&nbsp;the direction of the wind. The
                 purple line with the small circle on the end is the course over
                 ground. Its length represents how far the boat will travel in 10
-                minutes. Finally, the red boat is oriented along the
+                minutes. Finally, the boat is oriented along the
                 boat&apos;s&nbsp;<i>heading</i>. Putting it all together, in
-                this image, the boat is heading south-southwest, the wind is
-                between 3 and 8 knots from the west, and the course over ground
+                the image to the right, the boat is heading southwest, the wind is
+                between 8 and 12 knots from the west, and the course over ground
                 is south.
             </p>
-            <img src={"/flyer-map/Sample.png"}
-                 width="155"
-                 height="173"
-                 alt="Boat guide" />
         </>
     );
 };
@@ -36,7 +36,7 @@ export const About = () => {
     return (
         <div className="about">
             <MapGuide />
-            <h2>About</h2>
+            <h2 style={{clear:"right"}}>About</h2>
             <p>
                 Data originates on an NMEA 2000 network, flows through a{" "}
                 <a
